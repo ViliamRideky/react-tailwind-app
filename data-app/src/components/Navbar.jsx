4e62681d2ba58c99fb4b3cbc0637bcead4325057
ViliamRideky <rideky.viliam@gmail.com>
2025-02-03 21:24:35 +0100

    project initialization

---
+++ data-app/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="text-white">
+            <h1 className="w-full text-3xl font-bold text-[#d69f27]">TRY.</h1>
+        </div>
+    )
+} 
+
+export default Navbar
